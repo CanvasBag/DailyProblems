0b97ee5c7070a35f1100bd671da69e8f227f1274
CanvasBag <plsm.mail@gmail.com>
2019-03-21 21:37:03 +0000

    py #4 update

--- 1-10.py
+++ 1-10.py
@@ -144,3 +144,28 @@ node = Node('root', Node('left', Node('left.left')), Node('right'))
 assert Node.deserialize(Node.serialize(node)).left.left.val == 'left.left'
 
 # endregion
+
+# region Ex 4 - Find First Pos Integer
+
+# --------------------------------------------------------------
+# This problem was asked by Stripe.
+#
+# Given an array of integers, find the first missing positive integer in linear time and constant space. In other
+# words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and
+# negative numbers as well.
+#
+# For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+#
+# You can modify the input array in-place.
+# --------------------------------------------------------------
+
+def findFirstPosInteger(inputArray):
+    posInteger = inputArray[0]
+
+    for num in inputArray:
+        if num <= posInteger:
+            posInteger -= 1
+        else:
+
+
+    return posInteger
